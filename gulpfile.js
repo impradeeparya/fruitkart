0dc6122aef46790648ccaad493426d5a6620e5a3
Pradeep Arya <impradeeparya@gmail.com>
2017-03-24 20:52:31 +0530

    refactor structure and angularjs integration

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,41 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 var connect = require('gulp-connect');
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify');
+
+
+/*
+* files path
+*/
+var jsModules = [
+  'src/modules/*.js',
+  'src/modules/**/**/*.js'
+];
+
+var jsFiles = [
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/angular/angular.js',
+  'node_modules/angular-route/angular-route.js'
+];
+
+var htmlFiles = [
+  'src/*.html',
+  'src/***/**/*.html'
+];
+
+var jsDestination = 'dist/assests/js';
+
+
 
 /*
 * check js files for error
 */
 gulp.task('lint', function() {
-    return gulp.src(['src/app/scripts/*.js', 'src/app/scripts/**/**/*.js'])
+    return gulp.src(jsModules)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -16,23 +45,13 @@ gulp.task('lint', function() {
 * watch files for changes
 */
 gulp.task('watch', function() {
-    gulp.watch(['src/app/scripts/*.js', 'src/app/scripts/**/**/*.js'], ['lint']);
-});
-
-/*
-* deploy application to server
-*/
-gulp.task('connect', function () {
-  connect.server({
-    root: 'app/',
-    port: 8888
-  });
+    gulp.watch(modulesJS, ['lint']);
 });
 
 /*
 * Application deployment
 */
-gulp.task('connectDist', function () {
+gulp.task('connect', function () {
   connect.server({
     root: 'dist/',
     port: 9999
@@ -40,24 +59,78 @@ gulp.task('connectDist', function () {
 });
 
 /*
+* Application deployment dev
+*/
+gulp.task('dev-deploy', function () {
+  connect.server({
+    root: 'src/',
+    port: 9999
+  });
+});
+
+
+/*
 * clean dist folder
 */
 gulp.task('clean', function() {
-    gulp.src('./dist/*')
+    gulp.src('dist/*')
       .pipe(clean({force: true}));
 });
 
 /*
+* minify all third party js
+*/
+gulp.task('minify-js', function() {  
+    return gulp.src(jsFiles)
+        .pipe(concat('third-party.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDestination));
+});
+
+/*
+* copy html files to dist folder
+*/
+gulp.task('copy-html-files', function () {
+  gulp.src(htmlFiles)
+    .pipe(gulp.dest('dist/'));
+});
+
+
+/*
+* copy modules file
+*/
+gulp.task('copy-modules-files', function () {
+  gulp.src(jsModules)
+    .pipe(gulp.dest('dist/modules'));
+});
+
+
+
+/*
 * create default task
 */
-gulp.task('default', ['connect', 'lint', 'watch']);
+gulp.task('default', ['lint', 'watch']);
+
+/*
+* deploy application -> clean dist folder, check for error in files, copy files, deploy to server
+*/
+gulp.task('deploy', function() {
+  runSequence(
+    ['clean'],
+    ['lint', 'minify-js', 'copy-html-files', 'copy-modules-files'],
+    ['connect']
+  );
+});
 
 /*
 * build application -> clean and check for error in files
 */
 gulp.task('build', function() {
+  jsDestination = 'src/assests/js'
   runSequence(
     ['clean'],
-    ['lint', 'connectDist']
+    ['lint', 'minify-js'],
+    ['dev-deploy']
   );
 });
